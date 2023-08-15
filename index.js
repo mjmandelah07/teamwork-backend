8e3a6c58c7746b8c1f24e12c56daafebcc2962f5
mjmandelah07 <aramidemojisolagrace@gmail.com>
2023-08-15 07:13:11 +0100

    Connected to postgres database

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const db = require('./db/db');
 const express = require('express');
 
 
@@ -10,6 +10,19 @@ const PORT = process.env.PORT || 4000;
 // Define a route handler for the root URL
 app.get('/', (req, res) => {
   res.send('We are live now!');
+});
+
+// Define a route handler for the database connection url
+app.get('/test-db', async (req, res) => {
+  try {
+    await db.connect(); // Connect to the database
+    res.send('Connected to PostgreSQL database');
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  } finally {
+    db.end(); // Disconnect from the database
+  }
 });
 
 
