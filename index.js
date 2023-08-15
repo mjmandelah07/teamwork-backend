6c48ed2b8d775e12867e40a40b6c3d143f9c8fca
mjmandelah07 <aramidemojisolagrace@gmail.com>
2023-08-15 02:45:55 +0100

    created node for backend

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const express = require('express');
+
+
+
+// create a port to listen on
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Define a route handler for the root URL
+app.get('/', (req, res) => {
+  res.send('We are live now!');
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
