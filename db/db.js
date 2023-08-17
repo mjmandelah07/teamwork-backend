8322022738c57fae14e4a4a65e970773cbb9c77b
mjmandelah07 <aramidemojisolagrace@gmail.com>
2023-08-17 02:18:18 +0100

    created users endpoint to post and get

--- db/db.js
+++ db/db.js
@@ -26,6 +26,6 @@ module.exports = {
   },
   query: (query, values) => {
     return client.query(query, values);
-  }
- 
+  },
+  
 };
--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const { createUsersAccount } = require("./db/queries/createUsersAccount.js");
 const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes"); // Import the user routes module
+const db = require("./db/db.js");
+
 
 // create a port to listen on
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
 
 // Define a route handler for the root URL
 app.get("/", (req, res) => {
   res.send("We are live now!");
 });
 
-// connect to the database
-const database = async () => {
-  try {
-    // await db.connect();
-    await createUsersAccount();
-  } catch (err) {
-    console.error(err);
-  }
-};
-database();
+//  routes
+app.use("/api/users", userRoutes);
+
+
+// // connect to the database
+// const database = async () => {
+//   try {
+//     // await db.connect();
+//     await createUsersAccount();
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+// database();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
