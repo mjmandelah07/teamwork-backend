f7818de97e60c4e6f5bcaeba788d77fed5d4ce90
mjmandelah07 <aramidemojisolagrace@gmail.com>
2023-08-23 21:07:48 +0100

    created endpoint to delete gifs

--- controller/gif-controller.js
+++ controller/gif-controller.js
@@ -75,4 +75,54 @@ const getAllGifs = async (req, res) => {
   }
 };
 
-module.exports = { createGif, getAllGifs };
+const deleteGifById = async (req, res) => {
+  const userId = req.user?.id;
+  const userRole = req.user?.role;
+  const gifId = req.params.gifId;
+
+  try {
+    if (userRole !== "admin") {
+      const authorQuery = "SELECT user_id FROM gifs WHERE id = $1";
+      const authorResult = await db.query(authorQuery, [gifId]);
+
+      if (
+        authorResult.rows.length === 0 ||
+        authorResult.rows[0].user_id !== userId
+      ) {
+        return res.status(403).json({
+          status: "error",
+          error: "Access denied: You are not authorized to delete this gif",
+        });
+      }
+    }
+    const deleteQuery = `
+        DELETE FROM gifs
+        WHERE id = $1;
+      `;
+    const deleteValues = [gifId];
+    const deleteResult = await db.query(deleteQuery, deleteValues);
+
+    if (deleteResult.rowCount === 0) {
+      return res.status(404).json({
+        status: "error",
+        error: "Gif not found or not authorized to delete",
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        message: "gif post successfully deleted",
+      },
+    });
+  } catch (error) {
+    console.error("Error deleting Gif:", error);
+    res.status(500).json({
+      status: "error",
+      error: "An error occurred while deleting Gif",
+    });
+  }
+};
+
+
+module.exports = { createGif, getAllGifs, deleteGifById };
--- routes/gif-routes.js
+++ routes/gif-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createGif, getAllGifs } = require("../controller/gif-controller");
+const { createGif, getAllGifs, deleteGifById  } = require("../controller/gif-controller");
 
 const authMiddleware = require("../auth/auth-middle-ware");
 
@@ -9,6 +9,10 @@ router.post("/gifs", authMiddleware, createGif);
 
 // Route to get list of GIFs data
 router.get("/", getAllGifs);
+
+// employees can delete gifs
+router.delete("/gifs/:gifId", authMiddleware, deleteGifById);
+
 
 module.exports = router;
 
