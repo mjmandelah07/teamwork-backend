51edcf59929980890aaf82588cd6005020ec95bf
mjmandelah07 <aramidemojisolagrace@gmail.com>
2023-08-31 23:37:00 +0100

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/user-routes");
 const authRoutes = require("./routes/auth-routes");
