632b612ee7c333c7f3476bd90b119edc4c87ee96
mjmandelah07 <aramidemojisolagrace@gmail.com>
2023-09-01 02:50:56 +0100

    Added query to delete gif and article comments along side its article or gif

--- controller/gif-controller.js
+++ controller/gif-controller.js
@@ -135,6 +135,13 @@ const deleteGifById = async (req, res) => {
         );
     }
 
+    // Delete the comments associated with the gif
+    const deleteCommentsQuery = `
+        DELETE FROM gif_comments
+        WHERE gif_id = $1;
+        `;
+    await db.query(deleteCommentsQuery, [gifId]);
+
     // delete the gif from the server
     const deleteQuery = `
         DELETE FROM gifs
